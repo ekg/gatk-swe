61a9128bfa68892267ffa37dc72b34fdcfbfd8ec
pushkarev <dmitry@clusterk-dmitry.local>
2014-09-23 12:19:04 -0700

    combine_vcf now runs in parallel

--- combine_vcf/combine_vcf.sh
+++ combine_vcf/combine_vcf.sh
@@ -7,21 +7,36 @@ input=$(./swe get input)
 
 [ ! -e raw.vcf.tmp ] || rm raw.vcf.tmp
 
-# replace with parallel
-for vcf in $input
-do
-    local_vcf=$(./swe fetch $vcf)
+run_in_parallel=1
 
-    cat $local_vcf |grep -P '^\#' >header.vcf
-    cat $local_vcf  >> raw.vcf.tmp
+# cat all input VCFs into raw.vcf.tmp
+if [ "$run_in_parallel" != "" ]
+then
+    # save input files into input.XXXX.vcf in parallel
+    parallel -j 10 -i bash -c "mv \`./swe fetch {}\` input.\$\$.vcf " -- $input
 
-done
+    cat input.*.vcf > raw.vcf.tmp
+else
+    for vcf in $input
+    do
+	cat $(./swe fetch $vcf) >> raw.vcf.tmp
+    done
+fi
+
+
+#obtain header from the first VCF in the list
+set -- $input
+first_vcf=$1
+
+cat $(./swe fetch $first_vcf) |grep -P '^\#' >header.vcf
+
 
 cat header.vcf > raw.vcf
 cat raw.vcf.tmp | grep -vP "^\#" | ./vcf-sort -c >> raw.vcf
 
 bgzip -f raw.vcf 
 tabix -p vcf raw.vcf.gz
+
 
 ./swe emit file raw.vcf.gz
 ./swe emit file raw.vcf.gz.tbi
