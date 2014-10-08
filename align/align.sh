8a9dbbfa82e3811910d5efe177ae5e6376601fb3
Erik Garrison <erik.garrison@gmail.com>
2014-10-08 19:48:41 -0400

    remove sambamba from alignment step

--- align/align.sh
+++ align/align.sh
@@ -9,16 +9,11 @@ gatk_data=$(./swe get GATK_DATA)
 cpu_cores=32
 GROUP_ID="@RG\tID:1\tPL:ILLUMINA\tPU:pu\tLB:group1\tSM:Sample_XXX"
 
-sort_dir=$$.sambamba_sort_dir
-mkdir $sort_dir
 
 #samtools sort is limited to 62M per thread, so that it won't take more than 2GB total
 bwa mem -M -p -t $cpu_cores -R "$GROUP_ID" $gatk_data/hg19/ucsc.hg19.fasta $input \
-    | sambamba view -S -f bam -l 0 /dev/stdin \
-    | sambamba sort -t $cpu_cores -m $[2000/$cpu_cores]M --tmpdir=$sort_dir -o raw.bam /dev/stdin
-
-    #| samtools view -@ $cpu_cores -1 -bt   $gatk_data/hg19/ucsc.hg19.fasta.fai - \
-    #| samtools sort -@ $cpu_cores -m $[2000/$cpu_cores]M -l 0 - raw
+    | samtools view -@ $cpu_cores -1 -bt   $gatk_data/hg19/ucsc.hg19.fasta.fai - \
+    | samtools sort -@ $cpu_cores -m $[2000/$cpu_cores]M -l 0 - raw
 
 samtools index raw.bam
 
